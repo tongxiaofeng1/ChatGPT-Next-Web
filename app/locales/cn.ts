ce2451fc2c432da9ba22bc35564d5bddaa87e20f
tongxiaofeng1 <102214693+tongxiaofeng1@users.noreply.github.com>
2023-04-02 13:53:55 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -67,12 +67,12 @@ const cn = {
     },
 
     Update: {
-      Version: (x: string) => `当前版本：${x}`,
-      IsLatest: "已是最新版本",
-      CheckUpdate: "检查更新",
-      IsChecking: "正在检查更新...",
-      FoundUpdate: (x: string) => `发现新版本：${x}`,
-      GoToUpdate: "前往更新",
+      Version: (x: string) => `运营商`,
+      IsLatest: "",
+      CheckUpdate: "",
+      IsChecking: "",
+      FoundUpdate: (x: string) => ``,
+      GoToUpdate: "凖备中.",
     },
     SendKey: "发送键",
     Theme: "主题",
